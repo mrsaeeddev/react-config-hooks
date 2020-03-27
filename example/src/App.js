998a75b3266b5d7fbd1a8932257ffeed1a829a07
sa-js <philipthevictor@gmail.com>
2020-03-27 23:49:16 +0500

    improved example app according to additons in useOS hook

--- example/src/App.js
+++ example/src/App.js
@@ -3,12 +3,18 @@ import { useOS, useBrowser } from 'react-configurational-hooks'
 
 const App = () => {
   const [os,isWin,isMac,isUnix,isLinux,isWinMob,isAndroid,isiOS] = useOS();
-  console.log(os,isWin,isMac,isUnix,isLinux,isWinMob,isAndroid,isiOS)
   const browser = useBrowser();
   return (
     <div>
       <div>Your host : {os}</div>
       <div>Your browser : {browser}</div>
+      {isWin && <div>This is Windows OS</div>}
+      {isMac && <div>This is Mac OS</div>}
+      {isUnix && <div>This is Unix-based OS</div>}
+      {isLinux && <div>This is Linux OS</div>}
+      {isWinMob && <div>This is Windows Mobile OS</div>}
+      {isAndroid && <div>This is Android OS</div>}
+      {isiOS && <div>This is iOS</div>}
     </div>
   )
 }
