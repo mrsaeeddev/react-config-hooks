ad7b3ee2e986b7c2b88266514e15021561743d35
sa-js <philipthevictor@gmail.com>
2020-03-28 10:11:35 +0500

    updated example app according to booleans related to browser detection

--- example/src/App.js
+++ example/src/App.js
@@ -3,11 +3,11 @@ import { useOS, useBrowser } from 'react-configurational-hooks'
 
 const App = () => {
   const [os,isWin,isMac,isUnix,isLinux,isWinMob,isAndroid,isiOS] = useOS();
-  const browser = useBrowser();
+  const [browser,isChrome,isFirefox,isIE,isEdge,isSafari,isOpera,isYandex] = useBrowser();
   return (
     <div>
-      <div>Your host : {os}</div>
-      <div>Your browser : {browser}</div>
+      <h2>OS Configuration</h2>
+      <h4>Your host : {os}</h4>
       {isWin && <div>This is Windows OS</div>}
       {isMac && <div>This is Mac OS</div>}
       {isUnix && <div>This is Unix-based OS</div>}
@@ -15,6 +15,16 @@ const App = () => {
       {isWinMob && <div>This is Windows Mobile OS</div>}
       {isAndroid && <div>This is Android OS</div>}
       {isiOS && <div>This is iOS</div>}
+      <br />
+      <h2>Browser Configuration</h2>
+      <h4>Your browser: {browser}</h4>
+      {isChrome && <div>This is Google Chrome</div>}
+      {isFirefox && <div>This is Firefox</div>}
+      {isIE && <div>This is IE</div>}
+      {isEdge && <div>This is Edge</div>}
+      {isSafari && <div>This is Safari</div>}
+      {isOpera && <div>This is Opera</div>}
+      {isYandex && <div>This is Yandex</div>}
     </div>
   )
 }
--- src/browser.js
+++ src/browser.js
@@ -1,30 +1,43 @@
 import { useState, useEffect } from 'react'
 
 export const useBrowser = () => {
-  let [browser, setBrowser] = useState("unknown")
-
+  const [browser, setBrowser] = useState("unknown")
+  const [isChrome, setIsChrome] = useState(false)
+  const [isFirefox, setIsFirefox] = useState(false)
+  const [isIE, setIsIE] = useState(false)
+  const [isEdge, setIsEdge] = useState(false)
+  const [isSafari, setIsSafari] = useState(false)
+  const [isOpera, setIsOpera] = useState(false)
+  const [isYandex, setIsYandex] = useState(false)
 
   function checkBrowser() {
     if (navigator.userAgent.indexOf("Chrome") != -1 ) {
         setBrowser("Google Chrome");
+        setIsChrome(true);
       }
       else if (navigator.userAgent.indexOf("Firefox") != -1 ) {
         setBrowser("Mozilla Firefox");
+        setIsFirefox(true);
       }
       else if (navigator.userAgent.indexOf("MSIE") != -1 ) {
         setBrowser("Internet Explorer");
+        setIsIE(true);
       }
       else if (navigator.userAgent.indexOf("Edge") != -1 ) {
         setBrowser("Edge");
+        setIsEdge(true);
       }
       else if (navigator.userAgent.indexOf("Safari") != -1 ) {
         setBrowser("Safari");
+        setIsSafari(true);
       }
       else if (navigator.userAgent.indexOf("Opera") != -1 ) {
         setBrowser("Opera");
+        setIsOpera(true);
       }
       else if (navigator.userAgent.indexOf("Opera") != -1 ) {
         setBrowser("Yandex Browser");
+        setIsYandex(true);
       }
   }
 
@@ -32,5 +45,5 @@ export const useBrowser = () => {
       checkBrowser();
   }, [])
 
-  return browser
+  return [browser,isChrome,isFirefox,isIE,isEdge,isSafari,isOpera,isYandex]
 }
